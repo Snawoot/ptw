04a81d8708ce5fe80cf9cbf7ee8275c0d047dcee
Vladislav Yarmak <vladislav@vm-0.com>
2019-06-16 18:47:38 +0300

    simplify listener

--- ptw/listener.py
+++ ptw/listener.py
@@ -38,49 +38,32 @@ class Listener:  # pylint: disable=too-many-instance-attributes
             # after wait_closed() completed
             await asyncio.sleep(.5)
 
-    async def _pump(self, writer, reader, halt):
-        hlt_task = asyncio.ensure_future(halt.wait())
-        try:
-            while True:
-                rd_task = asyncio.ensure_future(reader.read(BUFSIZE))
-                try:
-                    done, pending = await asyncio.wait((rd_task, hlt_task),
-                                                       return_when=asyncio.FIRST_COMPLETED)
-                except asyncio.CancelledError:
-                    rd_task.cancel()
-                    hlt_task.cancel()
-                    raise
-                if hlt_task in done:
-                    rd_task.cancel()
-                    break
-                try:
-                    data = rd_task.result()
-                except ConnectionResetError:
-                    break
-                if not data:
-                    break
-                writer.write(data)
-                wr_task = asyncio.ensure_future(writer.drain())
-                try:
-                    done, pending = await asyncio.wait((wr_task, hlt_task),
-                                                       return_when=asyncio.FIRST_COMPLETED)
-                except asyncio.CancelledError:
-                    wr_task.cancel()
-                    hlt_task.cancel()
-                if hlt_task in done:
-                    wr_task.cancel()
-                    break
-        finally:
-            halt.set()
+    async def _pump(self, writer, reader):
+        while True:
+            try:
+                data = await reader.read(BUFSIZE)
+            except asyncio.CancelledError:
+                raise
+            except ConnectionResetError:
+                break
+            if not data:
+                break
+            writer.write(data)
+
+            try:
+                await writer.drain()
+            except ConnectionResetError:
+                break
+            except asyncio.CancelledError:
+                raise
 
     async def handler(self, reader, writer):
         peer_addr = writer.transport.get_extra_info('peername')
         self._logger.info("Client %s connected", str(peer_addr))
         try:
-            halt = asyncio.Event()
             dst_reader, dst_writer = await self._conn_pool.get() 
-            await asyncio.gather(self._pump(writer, dst_reader, halt),
-                                 self._pump(dst_writer, reader, halt))
+            await asyncio.gather(self._pump(writer, dst_reader),
+                                 self._pump(dst_writer, reader))
         except asyncio.CancelledError:  # pylint: disable=try-except-raise
             raise
         except Exception as exc:  # pragma: no cover
