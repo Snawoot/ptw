c8b39404296003ba9fb4a491c58b7ed67984f41f
Vladislav Yarmak <vladislav@vm-0.com>
2019-08-28 20:31:46 +0300

    prep for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='ptw',
-      version='0.4.2',
+      version='0.4.3',
       description='Pooling TLS Wrapper',
       url='https://github.com/Snawoot/ptw',
       author='Vladislav Yarmak',
